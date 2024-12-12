156af8246663c201ccd52388ed5da65094254328
willaCS <arthur.willems.prive@gmail.com>
2024-12-12 20:43:23 +0100

    remade framework as class

--- src/ui/framework/framework.py
+++ src/ui/framework/framework.py
@@ -1,145 +1,131 @@
+from typing import List
+
+
 emptyLambda = lambda: None
 
-def component(
-	rect,
-	margin=0,
-	padding=0,
-	z=0,
-	draw=emptyLambda,
-	click=emptyLambda,
-	clickOutside=emptyLambda,
-	childs=[]
-):
-	res = {
-		'_rect': rect,
-		'margin': margin,
-		'padding': padding,
-		'z': z,
+class Component:
+	def __init__(self,
+		rect,
+		margin=0,
+		padding=0,
+		z=0,
+		draw=emptyLambda,
+		click=emptyLambda,
+		clickOutside=emptyLambda,
+		childs=[] # type: List[Component]
+	):
+		self._rect = rect
+		self.margin = margin
+		self.padding = padding
+		self.z = z
 
-		'_draw': draw,
-		'_click': click,
-		'_clickOutside': clickOutside,
+		self._draw = draw
+		self._click = click
+		self._clickOutside = clickOutside
 
-		'_hidden': False,
-		'_temp': False,
+		self._hidden = False
+		self._temp = False
 
-		'_parent': None,
-		'_childs': childs,
-	}
-
-	for child in res['_childs']:
-		component_add_parent(child, res)
-	return res
+		self._parent = None # type: Component
+		self._childs = childs # type: List[Component]
+		for child in self._childs:
+			child.add_parent(self)
 	
-def component_show(component):
-	component['_hidden'] = False
+	def show(self):
+		self._hidden = False
 
-def component_hide(component):
-	component['_hidden'] = True
+	def hide(self):
+		self._hidden = True
 
-
-
-
-
-def _component_baseRect(component, parent_rect):
-	if callable(component['_rect']):
-		if component['_parent'] and component['_rect'].__code__.co_argcount >= 1:
-			return component['_rect'](parent_rect)
+	def _baseRect(self, parent_rect):
+		if callable(self._rect):
+			if self._parent and self._rect.__code__.co_argcount >= 1:
+				return self._rect(parent_rect)
+			else:
+				return self._rect()
 		else:
-			return component['_rect']()
-	else:
-		return component['_rect']
-
-
-def component_rect(component):
-	parent = component_child_rect(component['_parent']) if component['_parent'] else ((0, 0), (0, 0))
-	base = _component_baseRect(component, parent)
-	# print('base',base, component)
-	return (
-		(
-			parent[0][0] + base[0][0] + component['margin'],
-			parent[0][1] + base[0][1] + component['margin'],
-		),
-		(
-			base[1][0] - 2 * component['margin'],
-			base[1][1] - 2 * component['margin'],
-		),
-	)
-
-def component_child_rect(component):
-	parent = component_child_rect(component['_parent']) if component['_parent'] else ((0, 0), (0, 0))
-	base = _component_baseRect(component, parent)
-	return (
-		(
-			parent[0][0] + base[0][0] + component['margin'] + component['padding'],
-			parent[0][1] + base[0][1] + component['margin'] + component['padding'],
-		),
-		(
-			base[1][0] - 2 * (component['padding'] + component['margin']),
-			base[1][1] - 2 * (component['padding'] + component['margin']),
-		),
-	)
+			return self._rect
 	
+	def rect(self):
+		parent = self._parent.child_rect() if self._parent else ((0, 0), (0, 0))
+		base = self._baseRect(parent)
+		return (
+			(
+				parent[0][0] + base[0][0] + self.margin,
+				parent[0][1] + base[0][1] + self.margin,
+			),
+			(
+				base[1][0] - 2 * self.margin,
+				base[1][1] - 2 * self.margin,
+			),
+		)
+	
+	def child_rect(self):
+		parent = self._parent.child_rect() if self._parent else ((0, 0), (0, 0))
+		base = self._baseRect(parent)
+		return (
+			(
+				parent[0][0] + base[0][0] + self.margin + self.padding,
+				parent[0][1] + base[0][1] + self.margin + self.padding,
+			),
+			(
+				base[1][0] - 2 * (self.padding + self.margin),
+				base[1][1] - 2 * (self.padding + self.margin),
+			),
+		)
+	
+	def draw(self, mouse_position):
+		if self._hidden:
+			return	
+		rect = self.rect()
+		if self._draw.__code__.co_argcount >= 2:
+			self._draw(rect, self.is_in(mouse_position))
+		else:
+			self._draw(rect)
+		self._childs.sort(key=lambda c: c.z)
+		for child in self._childs:
+			child.draw(mouse_position)
 
 
+	def is_in(self, pos):
+		rect = self.rect()
+		return rect[0][0] <= pos[0] <= rect[0][0] + rect[1][0]\
+			and rect[0][1] <= pos[1] <= rect[0][1] + rect[1][1]
 
-
-
-
-def component_draw(component, mouse_position):
-	if component['_hidden']:
-		return	
-	rect = component_rect(component)
-	if component['_draw'].__code__.co_argcount >= 2:
-		component['_draw'](rect, component_is_in(component, mouse_position))
-	else:
-		component['_draw'](rect)
-	component['_childs'].sort(key=lambda c: c['z'])
-	for child in component['_childs']:
-		component_draw(child, mouse_position)
-
-def component_is_in(component, pos):
-	rect = component_rect(component)
-	return rect[0][0] <= pos[0] <= rect[0][0] + rect[1][0]\
-		and rect[0][1] <= pos[1] <= rect[0][1] + rect[1][1]
-
-def component_click(component, pos, has_already_clicked=False):
-	if component['_hidden']:
-		return False
-	component['_childs'].sort(key=lambda c: c['z'], reverse=True)
-	for child in component['_childs']:
-		if component_click(child, pos, has_already_clicked):
+	def click(self, pos, has_already_clicked=False):
+		if self._hidden:
+			return False
+		self._childs.sort(key=lambda c: c.z, reverse=True)
+		for child in self._childs:
+			if child.click(pos, has_already_clicked):
+				has_already_clicked = True
+		if self._click is not None and self.is_in(pos):
+			if has_already_clicked:
+				return has_already_clicked
 			has_already_clicked = True
-	if component['_click'] is not None and component_is_in(component, pos):
-		if has_already_clicked:
-			return has_already_clicked
-		has_already_clicked = True
-		if component['_click'].__code__.co_argcount >= 1:
-			component['_click'](pos)
+			if self._click.__code__.co_argcount >= 1:
+				self._click(pos)
+			else:
+				self._click()
 		else:
-			component['_click']()
-	else:
-		if component['_clickOutside'].__code__.co_argcount >= 1:
-			component['_clickOutside'](pos)
-		else:
-			component['_clickOutside']()
-	return has_already_clicked
+			if self._clickOutside.__code__.co_argcount >= 1:
+				self._clickOutside(pos)
+			else:
+				self._clickOutside()
+		return has_already_clicked
 
-def component_add_parent(component, parent):
-	component['_parent'] = parent
+	def add_parent(self, parent):
+		self._parent = parent
 
+	def add_temp(self,
+		new_childs # type: List[Component]
+	):  
+		for child in new_childs:
+			if not child:
+				continue
+			child._temp = True
+			self._childs.append(child)
+			child.add_parent(child, self)
 
-
-
-
-
-def component_add_temp(component, new_childs):
-	for child in new_childs:
-		if not child:
-			continue
-		child['_temp'] = True
-		component['_childs'].append(child)
-		component_add_parent(child, component)
-
-def component_temp_remove(component):
-    component['_childs'] = [child for child in component['_childs'] if not child['_temp']]
+	def temp_remove(self):
+		self._childs = [child for child in self._childs if not child._temp]
